c9d8ff5d7b4908020023fda5d0e1004bae5764a0
Holysaiyan <53127211+Holysaiyan@users.noreply.github.com>
2023-12-14 14:14:08 +0000

    Update index.js
    FastAPi Implementation

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
-let recipeForm = document.getElementById("recipeForm");
-let recipeName = document.getElementById("recipeName");
-let ingredients = document.getElementById("ingredients");
-let image_url = document.getElementById("recipeImage");
-let steps = document.getElementById("recipeSteps");
-let displayArea = document.getElementById("recipeDisplay");
-
-window.onload = function () {
-  if (localStorage.getItem("recipes")) {
-    recipes = JSON.parse(localStorage.getItem("recipes"));
-    displayRecipe(recipes);
-  }
-};
+let recipeForm = document.getElementById('recipeForm');
+let recipeName = document.getElementById('recipeName');
+let ingredients = document.getElementById('ingredients');
+let image_url = document.getElementById('recipeImage');
+let steps = document.getElementById('recipeSteps');
+let displayArea = document.getElementById('recipeDisplay');
 
 let recipes = [];
-recipeForm.addEventListener("submit", function (event) {
+
+async function read_recipe() {
+  let response = await fetch('http://127.0.0.1:8000/recipes', {
+    method: 'GET',
+  });
+  let data = await response.json();
+  return data;
+}
+
+read_recipe().then((data) => {
+  recipes = data;
+  displayRecipe(recipes);
+});
+
+recipeForm.addEventListener('submit', function (event) {
   event.preventDefault();
 
   let enteredName = recipeName.value;
@@ -28,38 +35,69 @@ recipeForm.addEventListener("submit", function (event) {
     image: enteredImageUrl,
   };
 
-  recipes.push(newRecipe);
+  create_recipe(newRecipe).then((data) => {
+    recipes.push(data);
+  });
 
-  // reset the values
-  recipeName.value = "";
-  ingredients.value = "";
-  steps.value = "";
-  image_url.value = "";
-
-  saveToLocalStorage();
-
-  displayRecipe(recipes);
+  resetForm();
 });
 
+let create_recipe = async (new_recipe) => {
+  try{
+    let response = await fetch("http://127.0.0.1:8000/recipes", {method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;'
+    },
+    body: JSON.stringify(new_recipe)})
+    if (response.ok){
+      let result = await response.json();
+      return result
+    }
+    else{
+      throw new Error(`Error: ${response.status} - ${response.statusText}`);
+    }
+  }catch(error){console.error('Error:', error.message);
+  throw error; }}
+
+function resetForm() {
+  recipeName.value = '';
+  ingredients.value = '';
+  steps.value = '';
+  image_url.value = '';
+}
+
 function displayRecipe(recipes) {
-  displayArea.innerHTML = ""; // Clear previous content
+  displayArea.innerHTML = ''; // Clear previous content
 
   recipes.forEach(function (recipe, index) {
-    let recipeDiv = document.createElement("div");
+    let recipeDiv = document.createElement('div');
     recipeDiv.innerHTML = `<img src="${recipe.image}" alt="${recipe.name}">
                            <h3>${recipe.name}</h3>
                            <p>Ingredients: ${recipe.ingredients}</p>
                            <p>Steps: ${recipe.steps}</p>`;
     displayArea.appendChild(recipeDiv);
 
-    let deleteButton = document.createElement("button");
-    deleteButton.textContent = "Delete";
+    // Delete button
 
-    deleteButton.addEventListener("click", function () {
+    let deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+
+    deleteButton.addEventListener('click', function () {
       deleteRecipe(index);
     });
 
     recipeDiv.appendChild(deleteButton);
+
+    // Edit Button
+
+    let editButton = document.createElement('button');
+    editButton.textContent = 'Edit';
+    editButton.style.backgroundColor = 'Green';
+
+    editButton.addEventListener('click', function () {
+      editRecipe(index);
+    });
+    recipeDiv.appendChild(editButton);
   });
 }
 
@@ -67,27 +105,28 @@ function displayRecipe(recipes) {
 function openModal(index) {
   const recipe = recipes[index];
 
-  const modalContent = document.getElementById("modalContent");
+  const modalContent = document.getElementById('modalContent');
   modalContent.innerHTML = `
+
   <img src="${recipe.image}" alt="${recipe.name}" />
     <h2>${recipe.name}</h2>
     <p><strong>Ingredients:</strong> ${recipe.ingredients}</p>
     <p><strong>Steps:</strong> ${recipe.steps}</p>
   `;
 
-  const modal = document.getElementById("recipeModal");
-  modal.style.display = "flex";
+  const modal = document.getElementById('recipeModal');
+  modal.style.display = 'flex';
 }
 
 // Function to close the modal
 function closeModal() {
-  const modal = document.getElementById("recipeModal");
-  modal.style.display = "none";
+  const modal = document.getElementById('recipeModal');
+  modal.style.display = 'none';
 }
 
 // Attach the openModal function to each recipe card click event
-displayArea.addEventListener("click", function (event) {
-  if (event.target.tagName === "IMG") {
+displayArea.addEventListener('click', function (event) {
+  if (event.target.tagName === 'IMG') {
     const index = Array.from(
       event.target.parentNode.parentNode.children
     ).indexOf(event.target.parentNode);
@@ -97,21 +136,62 @@ displayArea.addEventListener("click", function (event) {
 
 // Attach the closeModal function to the modal close button
 document
-  .getElementById("modalCloseButton")
-  .addEventListener("click", closeModal);
+  .getElementById('modalCloseButton')
+  .addEventListener('click', closeModal);
 
 function deleteRecipe(index) {
   // Remove the recipe at the specified index
   recipes.splice(index, 1);
 
-  // Save to local storage after deleting a recipe
-  saveToLocalStorage();
-
   // Update the display to reflect the changes
   displayRecipe(recipes);
 }
 
-function saveToLocalStorage() {
-  console.log("Saving to local storage:", recipes);
-  localStorage.setItem("recipes", JSON.stringify(recipes));
+function editRecipe(index) {
+  const recipe = recipes[index];
+
+  const modalContent = document.getElementById('modalContent');
+  modalContent.innerHTML = `
+    <form id="updateForm">
+      <label for="updateRecipeName">Recipe Name:</label>
+      <input type="text" id="updateRecipeName" name="updateRecipeName" value="${recipe.name}" />
+
+      <label for="updateIngredients">Ingredients:</label>
+      <textarea id="updateIngredients" name="updateIngredients">${recipe.ingredients}</textarea>
+
+      <label for="updateRecipeSteps">Steps for Preparation:</label>
+      <textarea id="updateRecipeSteps" name="updateRecipeSteps">${recipe.steps}</textarea>
+
+      <label for="updateRecipeImage">Recipe Image URL:</label>
+      <input type="url" id="updateRecipeImage" name="updateRecipeImage" value="${recipe.image}" />
+
+      <button style="background-color: green;" type="button" onclick="updateRecipe(${index})">Update Recipe</button>
+    </form>
+  `;
+
+  const modal = document.getElementById('recipeModal');
+  modal.style.display = 'flex';
+}
+
+function updateRecipe(index) {
+  // Get the updated values from the form
+  let updatedName = document.getElementById('updateRecipeName').value;
+  let updatedIngredients = document.getElementById('updateIngredients').value;
+  let updatedSteps = document.getElementById('updateRecipeSteps').value;
+  let updatedImageUrl = document.getElementById('updateRecipeImage').value;
+
+  // Update the recipe in the array
+  recipes[index] = {
+    name: updatedName,
+    ingredients: updatedIngredients,
+    steps: updatedSteps,
+    image: updatedImageUrl,
+  };
+
+  // Update the display to reflect the changes
+  displayRecipe(recipes);
+
+  // Reset the form and close the modal
+  resetForm();
+  closeModal();
 }
